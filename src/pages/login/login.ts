5be89e6b8e36666f6108bf51ab4ef90171f936fb
Tomas Piaggio <tomas.piaggio12@gmail.com>
2017-10-12 21:30:48 -0300

    Project not broke

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
 import {NavController} from "ionic-angular";
 import {HTTP} from "@ionic-native/http";
 import {URLBuilderService} from "../../app/services/url-builder.service";
-import {EncryptedHttpService} from "../../app/services/encrypted-http.service";
 
 @Component({
   selector: 'page-login',
@@ -20,8 +19,8 @@ export class LoginPage {
   }
 
   login(email: string, password: string): void {
-    // this.http.post(this.urlBuilder.buildURL('users'), JSON.stringify({user: email, password: password}), {})
-    //   .then((data) => console.log(data))
-    //   .catch(err => console.log(err));
+    this.http.post(this.urlBuilder.buildURL('users'), JSON.stringify({user: email, password: password}), {})
+      .then((data) => console.log(data))
+      .catch(err => console.log(err));
   }
 }
